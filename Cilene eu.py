41e05723ac8ce6b912c91b009e3a49f53f294ca2
Lindolfo Cavalcanti <89228680+LindolfoCavalcanti@users.noreply.github.com>
2022-03-19 22:35:45 -0300

    Update Cilene eu.py

--- Cilene eu.py
+++ Cilene eu.py
@@ -1,10 +1,4 @@
-total = int(input('Quantos verbos?'))
-quant_eu = int(input('Quantos eu?'))
-quant_tu = int(input('Quantos tu?'))
-quant_ele = int(input('Quantos ele?'))
-quant_nos = int(input('Quantos nos?'))
-quant_vos = int(input('Quantos vos?'))
-quant_eles = int(input('Quantos eles?'))
-
-percentual_eu = (quant_eu / total) * 100
-print('{:.2f}'.format(percentual_eu))
+desejado = int(input('Número de variável desajado: '))
+total = int(input('Numero total:'))
+porcentagem = (desejado / total) * 100
+print('{:.2f}'.format(porcentagem))
