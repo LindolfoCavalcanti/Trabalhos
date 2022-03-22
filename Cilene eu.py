39f6f72998fe0031141274d3c95560427bf908b2
Lindolfo Cavalcanti <89228680+LindolfoCavalcanti@users.noreply.github.com>
2022-03-22 10:45:44 -0300

    Update Cilene eu.py

--- Cilene eu.py
+++ Cilene eu.py
@@ -1,4 +1,4 @@
 desejado = int(input('Número de variável desajado: '))
 total = int(input('Numero total:'))
 porcentagem = (desejado / total) * 100
-print('{:.2f}'.format(porcentagem))
+print('{:.2f}%'.format(porcentagem))
